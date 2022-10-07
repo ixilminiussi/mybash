de0f2080664fa162d1edb71473f9dd8105b708ab
Ixil Miniussi <ixil.miniussi@outlook.fr>
2022-10-07 15:41:53 +0100

    $1

---
+++ custom_bash_commands.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# greet the user
+function greet() {
+	echo 'Hello '$1'!'
+}
+
+# basic git add commit push
+function lazygit() {
+	git add .
+	git commit -m '$1'
+	git push
+}
+
+#idle snake
+alias sandworm='ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#03045e,0.5:#0077b6,1:#90e0ef'
+
+# basic full upgrade
+alias update='sudo apt update && sudo apt full-upgrade && sudo apt autoremove'
+
+# Krita with tablet fix
+alias krita_='QT_XCB_TABLET_LEGACY_COORDINATES="" krita'
+
+# POETS boxes
+function poet() {
+	echo -n 'Enter the name of the poet: (ENTER for ayres)'
+	read POET
+	
+	case $POET in
+	ayres)
+		ssh -i .ssh/id_ed25519 idm1u19@ayres.cl.cam.ac.uk
+	;;
+	
+	*)
+		ssh -i .ssh/id_ed25519 idm1u19@ayres.cl.cam.ac.uk
+	;;
+esac
+}
+
+# list installed packages
+alias list-installed="comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u"
---
+++ startup_bash_commands.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#03045e,0.5:#0077b6,1:#90e0ef
